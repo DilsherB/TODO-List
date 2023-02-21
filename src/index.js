7f66f32307b40d5b2a0235c9d3bebfe65c0990dd
Dilsher Balouch <dilsherb@gmail.com>
2023-02-21 15:21:28 +0300

    list structure finalized

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
 import './style.css';
+
+let tasks = [
+  { description: 'Wash the dishes', completed: false, index: 1 },
+  { description: 'Complete to do list Project', completed: true, index: 2 },
+];
+
+const list = document.querySelector('#listItems');
+tasks.forEach((task) => {
+  if (task.completed) {
+    list.innerHTML += `<li class="deleteIcon">
+    <span>
+      <input type="checkbox" id="${task.index}" checked> 
+      <span class="lm">${task.description}</span>
+    </span>
+    <span class="center">
+      <i class="fa-solid fa-ellipsis-vertical dots"></i>
+      <i class="fa-solid fa-trash-can hidden"></i>
+    </span>
+    </li>`
+  } else {
+    list.innerHTML += `<li class="deleteIcon">
+    <span>
+      <input type="checkbox" id="${task.index}"> 
+      <span class="lm">${task.description}</span>
+    </span>
+    <span class="center">
+      <i class="fa-solid fa-ellipsis-vertical dots"></i>
+      <i class="fa-solid fa-trash-can hidden"></i>
+    </span>
+    </li>`
+  }
+});
+
+let dots = document.querySelectorAll('.dots');
+let trash = document.querySelectorAll('.fa-trash-can');
+let deleteIcon = document.querySelectorAll('.deleteIcon');
+for(let i=0; i<deleteIcon.length; i++) {
+  deleteIcon[i].addEventListener('mouseover', () => {
+    dots[i].classList.add('hidden');
+    trash[i].classList.remove('hidden');
+  });
+  deleteIcon[i].addEventListener('mouseout', () => {
+    dots[i].classList.remove('hidden');
+    trash[i].classList.add('hidden');
+  });
+}
